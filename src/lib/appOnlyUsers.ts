e2a3034cebd8e1fee49a1ad4c21df12e49dc5941
sotbjof <sotbjof@googlemail.com>
2025-12-18 23:19:05 +0000

    Fix TypeScript type error in APP_ONLY_USER_IDS

--- src/lib/appOnlyUsers.ts
+++ src/lib/appOnlyUsers.ts
@@ -7,10 +7,10 @@
  * 
  * Note: This is a workaround until we have a proper database flag for app-only users.
  */
-export const APP_ONLY_USER_IDS = [
+export const APP_ONLY_USER_IDS: string[] = [
   '4542c037-5b38-40d0-b189-847b8f17c222', // Jof
   'f8a1669e-2512-4edf-9c21-b9f87b3efbe2', // Carl
   '9c0bcf50-370d-412d-8826-95371a72b4fe', // SP
   '36f31625-6d6c-4aa4-815a-1493a812841b'  // ThomasJamesBird
-] as const;
+];
 
