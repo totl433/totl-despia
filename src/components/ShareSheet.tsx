287a04b3fb7351c73b928c04fed5cf803c445fb4
sotbjof <sotbjof@googlemail.com>
2025-12-27 17:46:37 +0000

    Remove aspectRatio constraint from share image to allow proper scaling

--- src/components/ShareSheet.tsx
+++ src/components/ShareSheet.tsx
@@ -188,7 +188,15 @@ export default function ShareSheet({
 
         {/* Generated image preview - flex to fill available space */}
         <div className="px-4 flex-1 min-h-0 flex items-center justify-center overflow-hidden py-2">
-          <div className="relative rounded-2xl overflow-hidden bg-white flex items-center justify-center" style={{ width: 'auto', height: 'auto', maxWidth: '100%', maxHeight: '100%' }}>
+          <div className="relative rounded-2xl overflow-hidden bg-white flex items-center justify-center" style={{ 
+            width: 'auto', 
+            height: 'auto', 
+            maxWidth: '100%', 
+            maxHeight: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
             <img
               src={imageUrl}
               alt={`Gameweek ${gw} predictions`}
@@ -198,8 +206,7 @@ export default function ShareSheet({
                 width: 'auto',
                 height: 'auto',
                 objectFit: 'contain',
-                display: 'block',
-                aspectRatio: '2/3'
+                display: 'block'
               }}
             />
             {/* Page indicators */}
