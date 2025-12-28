1e79378572d4eb0bf234f1bc695dbbdb83a01982
sotbjof <sotbjof@googlemail.com>
2025-12-28 19:05:47 +0000

    remove: share icon from score pill in capture component

--- src/components/GameweekFixturesCardListForCapture.tsx
+++ src/components/GameweekFixturesCardListForCapture.tsx
@@ -282,11 +282,6 @@ export default function GameweekFixturesCardListForCapture({
                   }}
                 ></div>
               )}
-              {allFinished && (
-                <svg className="fill-none stroke-current" fill="none" stroke="currentColor" viewBox="0 0 24 24" style={{ width: '14.63px', height: '14.63px' }}>
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                </svg>
-              )}
               <span className="flex items-baseline" style={{ display: 'flex', alignItems: 'baseline', gap: '2.09px' }}>
                 <span className="font-extrabold" style={{ fontSize: '18.81px', fontWeight: '800' }}>{score}</span>
                 <span className="font-medium opacity-90" style={{ fontSize: '14.63px', fontWeight: '500', opacity: 0.9 }}>/</span>
